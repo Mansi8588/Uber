94d3ec90b04d4afdff3fdbe35c301007f861c9e9
Mansi8588 <ajmrhw12@gmail.com>
2025-05-02 22:20:39 +0530

    pichup on homepage

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -1,11 +1,159 @@
-import React from 'react'
 
+
+import React, { useEffect, useRef, useState } from 'react'
+import {useGSAP} from '@gsap/react'
+import axios from 'axios';
+import gsap from 'gsap'
+import { useContext } from 'react';
+import { UserDataContext } from '../context/UserContext';
+import { useNavigate } from 'react-router-dom';
+import LocationSearchPanel from '../components/LocationSearchPanel';
+import 'remixicon/fonts/remixicon.css'
 const Home = () => {
-  return (
-    <div>
-      
-    </div>
-  )
+   const [pickup ,setPickup]= useState('');
+   const [destination,setDestination]=useState('');
+   const [panelOpen,setPanelOpen]=useState(false);
+   const panelRef=useRef(null);
+   const panelCloseRef=useRef(null);
+
+
+
+
+   
+  const submitHandler=(e)=>{
+    e.previntDefault();
+
+
+  }
+
+  useGSAP(function () {
+    if (panelOpen) {
+        gsap.to(panelRef.current, {
+            height: '70%',
+            padding: 24
+            // opacity:1
+        })
+        gsap.to(panelCloseRef.current, {
+            opacity: 1
+        })
+    } else {
+        gsap.to(panelRef.current, {
+            height: '0%',
+            padding: 0
+            // opacity:0
+        })
+        gsap.to(panelCloseRef.current, {
+            opacity: 0
+        })
+    }
+}, [ panelOpen ])
+
+
+
+    return (
+        <div className='h-screen relative overflow-hidden'>
+            <img className='w-16 absolute left-5 top-5' src="https://upload.wikimedia.org/wikipedia/commons/c/cc/Uber_logo_2018.png" alt="" />
+            <div className='h-screen w-screen'>
+                {/* image for temporary use  */}
+                {/* <LiveTracking /> */}
+            </div>
+            <div className=' flex flex-col justify-end h-screen absolute top-0 w-full'>
+                <div className='h-[30%] p-6 bg-white relative'>
+
+                <h5 
+                ref={panelCloseRef} onClick={() => {
+                        setPanelOpen(false)
+                    }}
+                    className='
+                    absolute opacity-0 right-6 top-6 text-2xl'
+                    
+                    >
+                        <i className="ri-arrow-down-wide-line"></i>
+                    </h5>
+
+                    <h4 className='text-2xl font-semibold'>Find a trip</h4>
+                    <form className='relative py-3' 
+                    onSubmit={(e) => {
+                        submitHandler(e)
+                    }}
+                    >
+                         <div className="line absolute h-16 w-1 top-[50%] -translate-y-1/2 left-5 bg-gray-700 rounded-full"></div> 
+                        <input
+                            onClick={() => {
+                                setPanelOpen(true)
+                                setActiveField('pickup')
+                            }}
+                            value={pickup}
+                            // onChange={handlePickupChange}
+                            className='bg-[#eee] px-12 py-2 text-lg rounded-lg w-full'
+                            type="text"
+                            placeholder='Add a pick-up location'
+                        />
+                        <input
+                            onClick={() => {
+                                setPanelOpen(true)
+                                setActiveField('destination')
+                            }}
+                            value={destination}
+                            // onChange={handleDestinationChange}
+                            className='bg-[#eee] px-12 py-2 text-lg rounded-lg w-full  mt-3'
+                            type="text"
+                            placeholder='Enter your destination' />
+                    </form>
+                    {/* <button
+                        // onClick={findTrip}
+                        className='bg-black text-white px-4 py-2 rounded-lg mt-3 w-full'>
+                        Find Trip
+                    </button> */}
+                </div>
+                <div 
+                ref={panelRef} 
+                className= ' bg-pink-500 h-0'>
+                    <LocationSearchPanel
+                        // suggestions={activeField === 'pickup' ? pickupSuggestions : destinationSuggestions}
+                        // setPanelOpen={setPanelOpen}
+                        // setVehiclePanel={setVehiclePanel}
+                        // setPickup={setPickup}
+                        // setDestination={setDestination}
+                        // activeField={activeField}
+                    />
+                </div>
+            </div>
+            {/* <div
+            //  ref={vehiclePanelRef} 
+             className='fixed w-full z-10 bottom-0 translate-y-full bg-white px-3 py-10 pt-12'>
+                <VehiclePanel
+                    selectVehicle={setVehicleType}
+                    fare={fare} setConfirmRidePanel={setConfirmRidePanel} setVehiclePanel={setVehiclePanel} />
+            </div>
+            <div ref={confirmRidePanelRef} className='fixed w-full z-10 bottom-0 translate-y-full bg-white px-3 py-6 pt-12'>
+                <ConfirmRide
+                    createRide={createRide}
+                    pickup={pickup}
+                    destination={destination}
+                    fare={fare}
+                    vehicleType={vehicleType}
+
+                    setConfirmRidePanel={setConfirmRidePanel} setVehicleFound={setVehicleFound} />
+            </div>
+            <div ref={vehicleFoundRef} className='fixed w-full z-10 bottom-0 translate-y-full bg-white px-3 py-6 pt-12'>
+                <LookingForDriver
+                    createRide={createRide}
+                    pickup={pickup}
+                    destination={destination}
+                    fare={fare}
+                    vehicleType={vehicleType}
+                    setVehicleFound={setVehicleFound} />
+            </div>
+            <div ref={waitingForDriverRef} className='fixed w-full  z-10 bottom-0  bg-white px-3 py-6 pt-12'>
+                <WaitingForDriver
+                    ride={ride}
+                    setVehicleFound={setVehicleFound}
+                    setWaitingForDriver={setWaitingForDriver}
+                    waitingForDriver={waitingForDriver} />
+            </div> */}
+        </div>
+    )
 }
 
 export default Home
