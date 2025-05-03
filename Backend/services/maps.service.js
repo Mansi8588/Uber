4f967dfb6793e4b17e349569b98c03abcaebb61b
Mansi8588 <ajmrhw12@gmail.com>
2025-05-04 03:17:14 +0530

    Creating suggestion , getCoordinates & distanceTime endpoint with google maps

---
+++ Backend/services/maps.service.js
@@ -0,0 +1,82 @@
+const axios = require('axios');
+const captainModel = require('../models/captain.model');
+
+require('dotenv').config();
+
+module.exports.getAddressCoordinate = async (address) => {
+    try {
+        const response = await axios.get('https://maps.gomaps.pro/maps/api/geocode/json', {
+            params: {
+                address,
+                key: process.env.GOOGLE_MAPS_API
+            }
+        });
+
+        if (response.data.status === 'OK') {
+            const location = response.data.results[0].geometry.location;
+            return {
+                ltd: location.lat,
+                lng: location.lng
+            };
+        } else {
+            console.error('Geocode failed:', response.data.status, response.data.error_message);
+            throw new Error('Unable to fetch coordinates');
+        }
+    } catch (error) {
+        console.error('Axios error:', error.message);
+        throw new Error('Unable to fetch coordinates');
+    }
+}
+
+module.exports.getDistanceTime = async (origin, destination) => {
+    if (!origin || !destination) {
+        throw new Error('Origin and destination are required');
+    }
+
+    const apiKey = process.env.GOOGLE_MAPS_API;
+
+    const url = `https://maps.gomaps.pro/maps/api/distancematrix/json?origins=${encodeURIComponent(origin)}&destinations=${encodeURIComponent(destination)}&key=${apiKey}`;
+
+    try {
+
+
+        const response = await axios.get(url);
+        if (response.data.status === 'OK') {
+
+            if (response.data.rows[ 0 ].elements[ 0 ].status === 'ZERO_RESULTS') {
+                throw new Error('No routes found');
+            }
+
+            return response.data.rows[ 0 ].elements[ 0 ];
+        } else {
+            throw new Error('Unable to fetch distance and time');
+        }
+
+    } catch (err) {
+        console.error(err);
+        throw err;
+    }
+}
+
+
+module.exports.getAutoCompleteSuggestions = async (input) => {
+    if (!input) {
+        throw new Error('query is required');
+    }
+
+    const apiKey = process.env.GOOGLE_MAPS_API;
+    const url = `https://maps.gomaps.pro/maps/api/place/autocomplete/json?input=${encodeURIComponent(input)}&key=${apiKey}`;
+
+    try {
+        const response = await axios.get(url);
+        if (response.data.status === 'OK') {
+            return response.data.predictions.map(prediction => prediction.description).filter(value => value);
+        } else {
+            throw new Error('Unable to fetch suggestions');
+        }
+    } catch (err) {
+        console.error(err);
+        throw err;
+    }
+}
+
